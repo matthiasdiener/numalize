33ceafd039d6aa6b88b8a6da025b839072001d98
Matthias Diener <mdiener@inf.ufrgs.br>
2013-11-14 17:06:20 +0100

    ifix order of tids

--- numalize.cpp
+++ numalize.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 #include <unordered_map>
+#include <map>
 
 #include "pin.H"
 
 const int MAXTHREADS = 128;
 
 struct pageinfo {
-	UINT64 accesses[MAXTHREADS];
+	map<UINT32, UINT64> accesses;
 	int firstacc;
 };
 
 unordered_map<UINT64, pageinfo> pagemap;
-PIN_LOCK mem_lock, tid_lock;
-int num_threads = 0;
+map<UINT32, char> pidmap;
+PIN_LOCK mem_lock;
 
 VOID memaccess(BOOL is_Read, ADDRINT pc, ADDRINT addr, INT32 size, THREADID threadid)
 {
+	int pid = PIN_GetTid();
 	UINT64 page = addr >> 12;
 
 	if (pagemap.find(page) == pagemap.end() ){
 		PIN_GetLock(&mem_lock, 0);
 		if (pagemap.find(page) == pagemap.end() )
-			pagemap[page].firstacc = threadid;
+			pagemap[page].firstacc = pid;
 		PIN_ReleaseLock(&mem_lock);
 	}
 
-	pagemap[page].accesses[threadid]++;
+	pagemap[page].accesses[pid]++;
 }
 
 
@@ -45,31 +47,31 @@ VOID trace_memory(INS ins, VOID *v)
 
 VOID ThreadStart(THREADID threadid, CONTEXT *ctxt, INT32 flags, VOID *v)
 {
-	PIN_GetLock(&tid_lock, threadid);
-
-	static int prev_pid = -1;
 	int pid = PIN_GetTid();
-	cout << "TID: " << threadid << " PID: " << pid << endl;
-	if (prev_pid>=pid)
-		cout << "XXX XXX XXX WRONG! XXX XXX XXX" << endl;
-	num_threads++;
-	prev_pid = pid;
-	PIN_ReleaseLock(&tid_lock);
+	pidmap[pid] = 1;
 }
 
 
 VOID Fini(INT32 code, VOID *v)
 {
+	int real_tid[MAXTHREADS];
+	int i = 0;
+
+	for (auto it = pidmap.cbegin(); it != pidmap.cend(); it++)
+		real_tid[i++] = it->first;
+
+	int num_threads = i;
+
 	UINT64 num_pages = 0;
 	cerr << "nr, addr, firstacc";
 	for (int i = 0; i<num_threads; i++)
 		cerr << ", T" << i;
 	cerr << endl;
 
-	for(auto it = pagemap.cbegin(); it != pagemap.cend(); it++) {
+	for(auto it = pagemap.begin(); it != pagemap.end(); it++) {
 		cerr << num_pages << ", " << it->first << ", " << it->second.firstacc;
 		for (int i=0; i<num_threads; i++) {
-			cerr << ", " << it->second.accesses[i];
+			cerr << ", " << it->second.accesses[real_tid[i]];
 		}
 		cerr << endl;
 		num_pages++;
@@ -82,7 +84,7 @@ VOID Fini(INT32 code, VOID *v)
 int main(int argc, char *argv[])
 {
 	if (PIN_Init(argc,argv)) {return 1;}
-	pagemap.reserve(1000000); //4GByte of mem usage, enough for NAS input C
+	pagemap.reserve(1000000); // 4GByte of mem usage, enough for NAS input C
 
 	PIN_AddThreadStartFunction(ThreadStart, 0);
 
@@ -90,7 +92,6 @@ int main(int argc, char *argv[])
 
 	PIN_AddFiniFunction(Fini, 0);
 	PIN_InitLock(&mem_lock);
-	PIN_InitLock(&tid_lock);
 
 	PIN_StartProgram();
 }
