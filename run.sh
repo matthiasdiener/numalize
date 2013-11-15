04c449e79e81bfe8f2c1dd16e633cf536c16ccdf
Matthias Diener <mdiener@inf.ufrgs.br>
2013-11-15 14:00:51 -0200

    fix csv creation

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 set -o errexit; set -o nounset
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-OUTFILE=$(basename ${@}).csv
+OUTFILE=$(basename ${1}).csv
 
 /opt/pin/pin -t $DIR/obj-intel64/*.so -- ${@} 2>$OUTFILE
+
+echo "created $OUTFILE"
