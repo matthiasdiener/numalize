d8b2b6bfb54e43e879762f37346c5184f3032661
Matthias Diener <mdiener@inf.ufrgs.br>
2013-11-17 14:22:25 -0200

    huge speedup

--- analyze.R
+++ analyze.R
@@ -12,12 +12,15 @@ paste0 <- function(..., sep = "") paste(..., sep = sep)
 library("treemap")
 library("RColorBrewer")
 library("plyr")
+library("parallel")
+library("data.table")
 
 args <- commandArgs(trailingOnly=TRUE)
 filename <- args[1:(length(args)-1)]
 outfilename <- gsub(".csv", ".pdf", filename)
 outfilename <- gsub(".gz", "", outfilename)
 nnodes <- as.numeric(args[length(args)])
+system("date")
 
 data <- NULL
 
@@ -29,7 +32,7 @@ for (i in 1:length(filename)) {
 	temp$name <- name
 	data <- rbind(data, temp)
 }
-
+system("date")
 nthreads <- length(grep("T[0-9]*", names(data)))
 cpn <- nthreads / nnodes
 
@@ -41,18 +44,28 @@ for (i in 0:(nnodes-1)) {
   data[paste("N",i,sep="")] <- rowSums(data[,(i*cpn+4):((i+1)*cpn+3)]) #NUmber of accesses per node
 }
 
+system("date")
 data$max <- apply(data[,(ncol(data)-nnodes+1):ncol(data)], 1, max) #Highest number of accesses
 
 data$excl <- data$max / data$sum * 100
 
-myround <- function(x){val <- round_any(x, 10, f=floor); val<-lapply(val, max, 30); lapply(val, min, 90)}
+system("echo -n before round ;date")
+#myround <- function(x){val <- round_any(x, 10, f=floor); val<-mclapply(val, max, 30); mclapply(val, min, 90)}
+data<-data.table(data)
+data$excl_round <- round_any(data$excl, 10)
+data$excl_round <- data[,min(excl_round,90), by=row.names(data)]$V1
+data$excl_round <- data[,max(excl_round,30), by=row.names(data)]$V1
 
-data$excl_round <- as.numeric(myround(data$excl))
+#data$excl_round <- as.numeric(myround(data$excl))
+system("echo -n after round ; date")
 
 data$excl_round <- paste(data$excl_round, "%", sep="")
 data$excl_round <- paste(ifelse(data$excl_round=="30%","<",""), data$excl_round, sep="")
 data$excl_round <- paste(ifelse(data$excl_round=="90%",">",""), data$excl_round, sep="")
 data <- transform(data, data.excl = factor(excl_round))
+
+print("Starting treemap")
+system("date")
 
 pdf(outfilename)
 
@@ -64,7 +77,7 @@ if (length(filename)>1) {
 
 options(warn=-1)
 
-treemap(data,
+treemap(data.frame(data),
 	index=ind,
 	vSize="sum",
 	vColor= "data.excl",
