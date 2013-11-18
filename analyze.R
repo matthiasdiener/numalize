246b372cc04e530ae8da58703a59ff507f181035
Matthias Diener <mdiener@inf.ufrgs.br>
2013-11-18 19:06:12 +0100

    small changes

--- analyze.R
+++ analyze.R
@@ -12,14 +12,14 @@ paste0 <- function(..., sep = "") paste(..., sep = sep)
 
 library(treemap)
 
+# Get command line arguments
 args <- commandArgs(trailingOnly=TRUE)
-filename <- args[1:(length(args)-1)]
-outfilename <- gsub(".csv", ".pdf", filename[1])
-outfilename <- gsub(".gz", "", outfilename)
+filenames <- args[1:(length(args)-1)]
+outfilename <- gsub(".gz", "", gsub(".csv", ".pdf", filenames[1]))
 nnodes <- as.numeric(args[length(args)])
 
 # Read csv files
-data <- do.call(rbind, lapply(filename, function(f) {
+data <- do.call(rbind, lapply(filenames, function(f) {
 	name <- toupper(gsub(".*/", "" , gsub("[.].*", "" , f)))
 	cat ("Loading", f, "=>", name, "\n")
 	temp <- read.csv(f)
@@ -61,12 +61,12 @@ pdf(outfilename)
 options(warn=-1)
 
 treemap(data,
-	index=if (length(filename)>1) c("name", "excl_round") else "excl_round",
+	index=if (length(filenames)>1) c("name", "excl_round") else "excl_round",
 	vSize="sum",
 	vColor= "data.excl",
 	type="categorical",
 	aspRatio=1,
-	palette="Greys",
+	palette="Greens",
 	# palette=c("#FFFFFF","#D2D2D2","#A8A8A8","#7E7E7E","#545454","#2A2A2A","#000000"),
 	title="",
 	title.legend="Exclusivity level",
