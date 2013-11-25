53400b47c974957a8440a6825b78acbb32b928a1
Matthias Diener <mdiener@inf.ufrgs.br>
2013-11-25 19:26:08 +0100

    script to convert csv to pdf

---
+++ mkCommMatrix.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+
+local({r <- getOption("repos"); r["CRAN"] <- "http://cran.r-project.org"; options(repos=r)})
+
+if (!suppressPackageStartupMessages(require(lattice))) {
+	install.packages(lattice)
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+filename <- args[1]
+outfilename <- gsub(".csv", ".pdf", filename)
+
+if (length(args) != 1) {
+	stop("Usage: mkCommMatrix.R <CommPattern.csv>\n")
+}
+
+csv <- read.csv(filename, header=FALSE)
+nt <- ncol(csv)
+
+rownames(csv) <- rev(as.integer(rownames(csv)) - 1)
+colnames(csv) <- rev(rownames(csv))
+
+mat <- data.matrix(csv)
+mat <- t(mat[nrow(mat):1,])
+
+pdf(outfilename)
+levelplot(mat, col.regions=grey(seq(1,0,-0.01)), colorkey=T, xlab="", ylab="", scales=list(x=list(at=seq(1,nt,5)), y=list(at=seq(1,nt,5))))
+garbage <- dev.off()
+
+system(paste("pdfcrop ", outfilename, outfilename, "> /dev/null"))
+
+cat("Generated", outfilename, "\n")
