734d7e07733bea7563fb003e8ef2bc0e2fa0505e
Matthias Diener <mdiener@inf.ufrgs.br>
2014-01-06 16:38:28 +0100

    make matrix for multiple input files

--- mkCommMatrix.R
+++ mkCommMatrix.R
@@ -8,26 +8,28 @@ if (!suppressPackageStartupMessages(require(lattice))) {
 }
 
 args <- commandArgs(trailingOnly=TRUE)
-filename <- args[1]
-outfilename <- gsub(".csv", ".pdf", filename)
 
-if (length(args) != 1) {
-	stop("Usage: mkCommMatrix.R <CommPattern.csv>\n")
+if (length(args) < 1)
+	stop("Usage: mkCommMatrix.R <CommPattern.csv>...\n")
+
+for (i in 1:length(args)) {
+	filename <- args[i]
+	outfilename <- gsub(".csv", ".pdf", filename)
+
+	csv <- read.csv(filename, header=FALSE)
+	nt <- ncol(csv)
+
+	rownames(csv) <- rev(as.integer(rownames(csv)) - 1)
+	colnames(csv) <- rev(rownames(csv))
+
+	mat <- data.matrix(csv)
+	mat <- t(mat[nrow(mat):1,])
+
+	pdf(outfilename)
+	print(levelplot(mat, col.regions=grey(seq(1,0,-0.01)), colorkey=F, xlab="", ylab="", scales=list(x=list(at=seq(1,nt,5)), y=list(at=seq(1,nt,5)))))
+	garbage <- dev.off()
+
+	system(paste("pdfcrop ", outfilename, outfilename, "> /dev/null"))
+
+	cat("Generated", outfilename, "\n")
 }
-
-csv <- read.csv(filename, header=FALSE)
-nt <- ncol(csv)
-
-rownames(csv) <- rev(as.integer(rownames(csv)) - 1)
-colnames(csv) <- rev(rownames(csv))
-
-mat <- data.matrix(csv)
-mat <- t(mat[nrow(mat):1,])
-
-pdf(outfilename)
-levelplot(mat, col.regions=grey(seq(1,0,-0.01)), colorkey=F, xlab="", ylab="", scales=list(x=list(at=seq(1,nt,5)), y=list(at=seq(1,nt,5))))
-garbage <- dev.off()
-
-system(paste("pdfcrop ", outfilename, outfilename, "> /dev/null"))
-
-cat("Generated", outfilename, "\n")
