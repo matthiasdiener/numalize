587d9d3790801c951562573b6a88b74db6329092
Matthias Diener <mdiener@inf.ufrgs.br>
2014-02-03 16:25:45 -0200

    compress and time

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ set -o errexit; set -o nounset
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTFILE=$(basename ${1}).csv
 
-/opt/pin/pin -t $DIR/obj-intel64/*.so -- ${@} 2>$OUTFILE
+time /opt/pin/pin -t $DIR/obj-intel64/*.so -- ${@} 2>$OUTFILE
+pigz --best $OUTFILE
 
-echo "created $OUTFILE"
+echo "created $OUTFILE.gz"
