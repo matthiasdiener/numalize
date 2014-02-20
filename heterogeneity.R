3ca23b4cd3f08d4c14cf05f57e1da18012859ca4
Matthias Diener <mdiener@inf.ufrgs.br>
2014-02-20 09:29:10 -0300

    accept multiple files

--- heterogeneity.R
+++ heterogeneity.R
@@ -1,22 +1,24 @@
 #!/usr/bin/env Rscript
 
-args <- commandArgs(trailingOnly=TRUE)
-filename <- args[1]
+filenames <- commandArgs(trailingOnly=TRUE)
 
-if (length(args) != 1) {
-	stop("Usage: heterogeneity.R <CommPattern.csv>\n")
+if (length(filenames) < 1)
+	stop("Usage: heterogeneity.R <CommPattern.csv>*\n")
+
+for (i in 1:length(filenames)) {
+	cat(filenames[i], "\n")
+	data <- read.csv(filenames[i], header=F)
+	nt <- length(data)
+
+	d <- data / max(data) * 100
+
+	v <- 0
+	for (i in 1:nt)
+		v <- v + var(d[-nt+1-i,i])
+
+	avg <- sum(data)/nt/nt
+
+	# cat("hf_old:", var(data)/sum(data)/nt/nt, "\n")
+	cat("hf_new:", v/nt, "\n")
+	cat("a:", avg, "\n")
 }
-
-data <- read.csv(filename, header=F)
-nt <- length(data)
-
-d <- data / max(data) * 100
-
-v <- 0
-for (i in 1:nt) {v=v+var(d[-nt+1-i,i])}
-
-avg <- sum(data)/nt/nt
-
-# cat("hf_old:", var(data)/sum(data)/nt/nt, "\n")
-cat("hf_new:", v/nt, "\n")
-cat("a:", avg, "\n")
