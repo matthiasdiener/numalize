bd5c60e3ff51792849af354bacb526bde1d21b0e
Matthias Diener <mdiener@inf.ufrgs.br>
2014-04-16 12:26:18 -0300

    new hf

--- heterogeneity.R
+++ heterogeneity.R
@@ -6,19 +6,23 @@ if (length(filenames) < 1)
 	stop("Usage: heterogeneity.R <CommPattern.csv>*\n")
 
 for (i in 1:length(filenames)) {
-	cat(filenames[i], "\n")
+	cat(filenames[i], " ")
 	data <- read.csv(filenames[i], header=F)
 	nt <- length(data)
 
-	d <- data / max(data) * 100
+	d <- (data / max(data))# * 100
+	# d[d<30] <- 0
+	d[d>0.3] <- 1
 
 	v <- 0
-	for (i in 1:nt)
+	for (i in 1:nt) {
 		v <- v + var(d[-nt+1-i,i])
+		# print (var(d[-nt+1-i,i]))
+	}
 
 	avg <- sum(as.numeric(unlist(data)))/nt/nt
 
-	# cat("hf_old:", var(data)/sum(data)/nt/nt, "\n")
-	cat("hf_new:", v/nt, "\n")
-	cat("a:", avg, "\n")
+	# cat(var(data)/sum(data)/nt/nt, "\n")
+	cat(v/nt, "\n")
+	# cat("a:", avg, "\n")
 }
