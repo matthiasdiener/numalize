aab35aca0f5cae842d7c751305d9d0b40b608edd
Matthias Diener <mdiener@inf.ufrgs.br>
2014-05-05 19:01:55 -0300

    cleanup page_dyn

--- page_dyn.R
+++ page_dyn.R
@@ -2,47 +2,37 @@
 
 paste0 <- function(..., sep = "") paste(..., sep = sep)
 
-args <- commandArgs(trailingOnly=TRUE)
-if (length(args) < 2)
-	stop("Usage: mkPageUp.R <page.csv>... <#nodes>\n")
+files <- list.files(pattern=".page.csv")
 
-filenames <- args[1:(length(args)-1)]
-# nnodes <- as.numeric(args[length(args)])
-
-# cn = data.frame()
-
-j=1
 nnodes=4
-# nthreads=64
 tpn=16
 # ttn <- floor(0:(nthreads-1))/tpn
 nodes <- paste0("N", 0:(nnodes-1))
-# threads <- paste0("T", 0:(nnodes-1))
 
-for (f in filenames) {
-	cat(f, "\n")
-
-	data=read.csv(f)
-	threads <- grep("T\\d+", names(data))
+addn = function(frame) {
+	threads <- grep("T\\d+", names(frame))
 
 	for (i in 0:(nnodes-1))
-		data[nodes[i+1]] <- rowSums(data[threads[(i*tpn+1):((i+1)*tpn)]])
+		frame[nodes[i+1]] <- rowSums(frame[threads[(i*tpn+1):((i+1)*tpn)]])
 
+	frame$cn = max.col(frame[nodes], ties.method="first")
+	frame=data.frame(frame$addr, frame$cn)
+	return(frame)
+}
 
-	# data$cn=apply(data[,nodes], 1, which.max)
-	data$cn = max.col(data[nodes], ties.method="first")
+tmp=list()
 
-	if (exists("cn")) {
-		tmp = data.frame(data$addr, data$cn)
-		names(tmp)[1] = "addr"
-		cn = merge(cn, tmp, all=TRUE, by= "addr")
-		names(cn)[ncol(cn)] = j
-	}
-	else {
-		cn =data.frame(data$addr, data$cn)
-		names(cn) = c("addr", 1)
-	}
-	j=j+1
+for (i in 1:length(files)) {
+	cat("Reading", files[i])
+	tmp[[i]]=addn(read.csv(files[i]))
+	cat(" done\n")
+}
+
+cn=tmp[[1]]
+
+for (i in 2:length(tmp)) {
+	cn=merge(cn, tmp[[i]], all=T, by=1)
+	names(cn)[ncol(cn)] = i
 }
 
 
@@ -71,5 +61,5 @@ res=data.frame(t(res))
 res[nrow(res)+1,]= colSums(res)
 res$sum <- rowSums(res)
 
-write.csv(res, file="full_page.csv")
+write.csv(res, file="full.csv")
 
