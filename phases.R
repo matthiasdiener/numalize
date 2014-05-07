433c3d62e3da2580f549ec1e78771ae02ad2850c
Matthias Diener <mdiener@inf.ufrgs.br>
2014-05-07 14:29:35 -0300

    cleanup

--- phases.R
+++ phases.R
@@ -4,44 +4,13 @@ library(parallel)
 
 fs <- list.files(pattern="^00.*\\.comm\\.csv$")
 
-doit = function(x) {
+read_scale = function(x) {
 	f = read.csv(x, header=F, quote="", colClasses="integer")
 	f[f>30] = 100
 	return (sum(apply(f, 1, var))/length(f))
 }
 
-# l = mclapply(fs, read.csv, header=F, quote="", colClasses="integer")
-l = mclapply(fs, doit, mc.cores=4)
-
-# for (i in 1:length(fs)) {
-# 	cat(fs[i], "\n")
-
-# 	l[[i]] = read.csv(fs[i], header=F, quote="", colClasses="integer")
-# 	# d=data.frame(scan("~/Dropbox/CommP/bt.csv", sep=",", quiet=T, multi.line=F))
-# # 	nt <- length(d)
-
-# # 	# d <- (data / max(data)) * 100
-# # 	# d[d<30] <- 0
-# 	# l[[i]][l[[i]]>30] <- 100
-
-# # 	#v <- 0
-# # 	#for (i in 1:nt) {
-# # #		v <- v + var(d[-nt+1-i,i])
-# # 		# print (var(d[-nt+1-i,i]))
-# # 	#}
-# # 	v=sum(apply(d,1,var))
-
-# # 	l=append(l, v/nt)
-# 	# avg <- sum(as.numeric(unlist(data)))/nt/nt
-
-# 	# cat("hf_old:", var(data)/sum(data)/nt/nt, "\n")
-
-# 	# cat(" ", v/nt, "\n")
-# 	# cat(" ", avg, "\n")
-# }
-
-
-# cat (min(l), max(l), var(l))
+l = mclapply(fs, read_scale, mc.cores=4)
 
 xx=split(unlist(l), ceiling(seq_along(l)/50))
 
@@ -65,6 +34,3 @@ for (x in m) {
 
 }
 cat ("n= ", n, "\n")
-
-# print (m)
-
