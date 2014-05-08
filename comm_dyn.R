0cb3e747653d9663243c25e2816c47fb1034c2cd
Matthias Diener <mdiener@inf.ufrgs.br>
2014-05-08 12:33:42 -0300

    create pdf

--- comm_dyn.R
+++ comm_dyn.R
@@ -1,22 +1,25 @@
 #!/usr/bin/env Rscript
 
+split=50
+
 library(parallel)
 
 options(mc.cores= as.numeric(system("grep 'processor' /proc/cpuinfo | sort | uniq | wc -l", intern=TRUE)))
 
 files <- list.files(pattern="\\.comm\\.csv$")
 
-read_scale = function(x) {
-	if (grepl("000\\.comm\\.csv$", x))
-		cat (x, "\n")
-	f = read.csv(x, header=F, quote="", colClasses="integer")
+read_scale = function(file) {
+	if (grepl("000\\.comm\\.csv$", file)) {
+		cat (file, format(Sys.time(), "%H:%M:%S"), "\n")
+	}
+	f = read.csv(file, header=F, quote="", colClasses="integer")
 	f[f>30] = 100
 	return (sum(apply(f, 1, var))/length(f))
 }
 
 het = mclapply(files, read_scale)
 
-het_split=split(unlist(het), ceiling(seq_along(het)/50))
+het_split=split(unlist(het), ceiling(seq_along(het)/split))
 
 het_mean = lapply(het_split, mean)
 
@@ -34,3 +37,11 @@ for (x in het_mean) {
 
 }
 cat ("\nn=", n, "\n")
+
+pdf("comm.pdf")
+
+
+plot(unlist(het), pch=20, ylim=c(0,max(unlist(het), na.rm=T)+100))
+points(seq(1, length(het_mean)*split, split), unlist(het_mean), pch=20)
+
+garbage <- dev.off()
