95e82826b0f3ee92299adcb1b69e7136efe4b7f2
Matthias Diener <mdiener@inf.ufrgs.br>
2014-05-08 17:52:54 -0300

    png output and avg

--- comm_dyn.R
+++ comm_dyn.R
@@ -1,27 +1,31 @@
 #!/usr/bin/env Rscript
 
-split=50
+split=500
 
 library(parallel)
 
-options(mc.cores= as.numeric(system("grep 'processor' /proc/cpuinfo | sort | uniq | wc -l", intern=TRUE)))
+options(mc.cores=as.numeric(system("grep 'processor' /proc/cpuinfo | sort | uniq | wc -l", intern=TRUE)))
 
 files <- list.files(pattern="\\.comm\\.csv$")
 
-read_scale = function(file) {
+read_het_avg = function(file) {
 	if (grepl("000\\.comm\\.csv$", file)) {
-		cat (file, format(Sys.time(), "%H:%M:%S"), "\n")
+		cat ("Reading", file, format(Sys.time(), "%H:%M:%S"), "\n")
 	}
 	f = read.csv(file, header=F, quote="", colClasses="integer")
 	f[f>30] = 100
-	return (sum(apply(f, 1, var))/length(f))
+	return(list(het=sum(apply(f, 1, var))/length(f), avg=sum(as.numeric(unlist(f)))/length(f) ))
 }
 
-het = mclapply(files, read_scale)
+res = mclapply(files, read_het_avg)
+het = unlist(res)[attr(unlist(res),"names")=="het"]
+avg = unlist(res)[attr(unlist(res),"names")=="avg"]
 
-het_split=split(unlist(het), ceiling(seq_along(het)/split))
+het_split = split(unlist(het), ceiling(seq_along(het)/split))
+avg_split = split(unlist(avg), ceiling(seq_along(avg)/split))
 
-het_mean = lapply(het_split, mean)
+het_mean = mclapply(het_split, mean)
+avg_mean = mclapply(avg_split, mean)
 
 xold=0
 n=0
@@ -38,10 +42,14 @@ for (x in het_mean) {
 }
 cat ("\nn=", n, "\n")
 
-pdf("comm.pdf")
+write.csv(data.frame(het, avg), "comm_dyn.csv")
 
+png("comm_dyn.png", width=960, res=100)
 
-plot(unlist(het), pch=20, ylim=c(0,max(unlist(het), na.rm=T)+100))
-points(seq(1, length(het_mean)*split, split), unlist(het_mean), pch=20)
+plot(unlist(het), pch=".", ylim=c(0,max(unlist(het),unlist(avg), na.rm=T)+100), xlab="Time", ylab="Het / Avg", col="gray")
+points(seq(1, length(het_mean)*split, split), unlist(het_mean), pch=20, col="black")
+
+points(unlist(avg), pch=".", col="lightgreen")
+points(seq(1, length(avg_mean)*split, split), unlist(avg_mean), pch=20, col="green")
 
 garbage <- dev.off()
