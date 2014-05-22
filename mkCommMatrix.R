11fa91fbd44c0afb8a7e101fbf3877ef5a5b8563
Matthias Diener <mdiener@inf.ufrgs.br>
2014-05-22 14:44:43 -0300

    accept comm matrices with spaces instead of commas

--- mkCommMatrix.R
+++ mkCommMatrix.R
@@ -1,29 +1,27 @@
 #!/usr/bin/env Rscript
 
-local({r <- getOption("repos"); r["CRAN"] <- "http://cran.r-project.org"; options(repos=r)})
+library(lattice)
+library(data.table)
 
-if (!suppressPackageStartupMessages(require(lattice))) {
-	install.packages("lattice")
-	library(lattice)
-}
-
-args <- commandArgs(trailingOnly=TRUE)
+args = commandArgs(trailingOnly=TRUE)
 
 if (length(args) < 1)
 	stop("Usage: mkCommMatrix.R <CommPattern.csv>...\n")
 
 for (i in 1:length(args)) {
-	filename <- args[i]
-	outfilename <- gsub(".csv", ".pdf", filename)
+	filename = args[i]
+	outfilename = gsub(".csv", ".pdf", filename)
+	if (filename == outfilename)
+		outfilename = paste(filename, ".pdf", sep="")
 
-	csv <- read.csv(filename, header=FALSE)
-	nt <- ncol(csv)
+	csv = as.data.frame(fread(filename, header=FALSE))
+	nt = ncol(csv)
 
-	rownames(csv) <- rev(as.integer(rownames(csv)) - 1)
-	colnames(csv) <- rev(rownames(csv))
+	rownames(csv) = rev(as.integer(rownames(csv)) - 1)
+	colnames(csv) = rev(rownames(csv))
 
-	mat <- data.matrix(csv)
-	mat <- t(mat[nrow(mat):1,])
+	mat = data.matrix(csv)
+	mat = t(mat[nrow(mat):1,])
 
 	pdf(outfilename)
 	print(levelplot(mat, col.regions=grey(seq(1,0,-0.01)), colorkey=F, xlab="", ylab="", scales=list(x=list(cex=1.5,at=seq(1,nt,5)), y=list(cex=1.5,at=seq(1,nt,5)))))
