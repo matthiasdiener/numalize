e73a2eb419c8a92148be0411b90fc3c272cca4cb
Matthias Diener <mdiener@inf.ufrgs.br>
2014-06-07 21:30:16 -0300

    add memory balance

--- mkPageUp.R
+++ mkPageUp.R
@@ -39,12 +39,38 @@ nodes = c(2:ncol(data))
 data$sum = rowSums(data[nodes])
 data$max = do.call(pmax, data[nodes])
 
-cat("#nodes:", nnodes, "  #threads:", nthreads, "  #threads per node:", tpn, "\n")
+cat("#nodes:", nnodes, "  #threads:", nthreads, "  #threads per node:", tpn, "\n\n")
 
 # first-touch correctness
 data$correct_node = max.col(data[nodes], "first")
 data$first_node = ttn[data$firstacc+1]
 data$firsttouch_acc = (data$correct_node == data$first_node) * data$sum
+
+# memory imbalance
+num_pages = nrow(data)
+data$rr_node = rep_len(c(1:nnodes), num_pages)
+
+cat("memory balance (pages), first-touch\n")
+table(data$first_node)
+cat("memory balance (pages), correct_node\n")
+table(data$correct_node)
+cat("memory balance (pages), rr_node\n")
+table(data$rr_node)
+
+total=sum(data$sum)
+
+cat("memory balance (accesses), first-touch\n")
+for (i in 1:nnodes)
+	cat(i, sum(data$sum[data$first_node==i])/total*100, "\n")
+
+cat("memory balance (accesses), correct_node\n")
+for (i in 1:nnodes)
+	cat(i, sum(data$sum[data$correct_node==i])/total*100, "\n")
+
+cat("memory balance (accesses), rr_node\n")
+for (i in 1:nnodes)
+	cat(i, sum(data$sum[data$rr_node==i])/total*100, "\n")
+
 
 # Exclusivity
 data$excl = data$max / data$sum * 100
@@ -60,4 +86,4 @@ DT[order(excl_round), sum(max), by=excl_round]
 
 cat("\nApp Exclusivity:", sum(data$max, na.rm=TRUE)/sum(data$sum, na.rm=TRUE)*100, "%\n")
 cat("First touch correctness (pages):", sum((data$correct_node == data$first_node))/nrow(data)*100, "%\n")
-cat("First touch correctness (scaled):", sum(data$firsttouch_acc, na.rm=TRUE)/sum(data$sum, na.rm=TRUE)*100, "%\n")
+cat("First touch correctness (accesses):", sum(data$firsttouch_acc, na.rm=TRUE)/sum(data$sum, na.rm=TRUE)*100, "%\n")
