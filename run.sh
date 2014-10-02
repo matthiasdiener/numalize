9c1d9338e99b940a3886b0979531c4eca7c80555
Matthias Diener <mdiener@inf.ufrgs.br>
2014-10-02 13:16:39 -0300

    perform page calculation in bc (supports fortran)

--- run.sh
+++ run.sh
@@ -9,20 +9,20 @@ DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROGARGS=$(echo ${@} | sed s,.*--\ ,,)
 OUTFILE=$(basename $PROGARGS | sed s,\\s.*,,).stackmap
 
-PAGE_SHIFT=$(echo l\($(getconf PAGESIZE)\)/l\(2\) | bc -l | sed s,\\..*,,)
+PAGESIZE=$(getconf PAGESIZE)
 
 cat > stack.gdb << EOF
 	break exit
 
 	run
 
-	thread apply all print (unsigned long)\$rsp >> $PAGE_SHIFT
+	thread apply all print/u \$rsp
 	continue
 EOF
 
 n=0
 
-gdb --batch --command=stack.gdb --args $PROGARGS | grep '^$.*=' | tac | cut -f 3 -d ' ' | while read line; do  echo $line|sed -e s,^,$n\ , ; n=$((++n)) ; done > $OUTFILE
+gdb --batch --command=stack.gdb --args $PROGARGS | grep '^$.*=' | tac | cut -f 3 -d ' ' | while read line; do  echo $line/$PAGESIZE | bc | sed -e s,^,$n\ , ; n=$((n+1)) ; done > $OUTFILE
 
 echo real stack
 cat $OUTFILE
