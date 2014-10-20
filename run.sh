a4c8525eb74b875a9c2e768fc6015b46ad4b984a
Matthias Diener <mdiener@inf.ufrgs.br>
2014-10-20 18:06:08 -0200

    make gdb work with openmp and pthreads

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ set -o errexit; set -o nounset
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 VA_RANDOM=$(sysctl -n kernel.randomize_va_space)
-if [ $VA_RANDOM -eq 0 ]; then
+if [ $VA_RANDOM -ne 0 ]; then
 	echo; echo "WARNING"
-	echo "sysctl kernel.randomize_va_space needs to be 0 (is $VA_RANDOM currently)"
+	echo "sysctl kernel.randomize_va_space needs to be 0 (it is $VA_RANDOM currently)"
 	echo "Generated information will be incorrect"
 	echo "WARNING"; echo
 fi
@@ -16,29 +16,57 @@ fi
 
 PROGARGS=$(echo ${@} | sed s,.*--\ ,,)
 PROG=$(echo $PROGARGS | { read first rest; echo $(basename $first) | sed s,\\s.*,, ; } )
-OUTFILE=$PROG.stackmap
+OUTFILE=$PROG.gdbstackmap
 
 PAGESIZE=$(getconf PAGESIZE)
 
+echo "Gathering stack information via gdb"
+
+rm -f gdb.txt
+
 cat > stack.gdb << EOF
+	python import os, math
+	python pagebits=int(math.log($PAGESIZE, 2))
+	python fname = os.path.basename(gdb.objfiles()[0].filename)
+	python f = open(fname + ".gdbstackmap", 'w')
+	python f2 = open(fname + ".gdbstackmap2", 'w')
+
+	catch syscall exit
+	commands 1
+		python print(gdb.selected_thread().num-1, int(gdb.parse_and_eval('\$rsp')) >> pagebits, file=f)
+		continue
+	end
+
+	catch syscall exit_group
+	commands 2
+		python print(gdb.selected_thread().num-1, int(gdb.parse_and_eval('\$rsp')) >> pagebits, file=f)
+		continue
+	end
+
 	break exit
+	commands 3
+		python for x in gdb.inferiors()[0].threads(): id=x.num; print(id); gdb.execute("thread " + str(id)); print(id-1, int(gdb.parse_and_eval('\$rsp')) >> pagebits, file=f2)
+		continue
+	end
 
 	run
 
-	thread apply all print/u \$rsp
-	continue
+	# python f.close()
+	# python f2.close()
+
 EOF
 
-echo "Gathering stack information via gdb"
+gdb --batch --command=stack.gdb --args $PROGARGS
 
-n=0
+# exit
+# n=0
+# cat gdb.txt | grep '^$.*=' | tac | cut -f 3 -d ' ' | while read line; do  echo $line/$PAGESIZE | bc | sed -e s,^,$n\ , ; n=$((n+1)) ; done | tee $OUTFILE
 
-gdb --batch --command=stack.gdb --args $PROGARGS | grep '^$.*=' | tac | cut -f 3 -d ' ' | while read line; do  echo $line/$PAGESIZE | bc | sed -e s,^,$n\ , ; n=$((n+1)) ; done > $OUTFILE
+# rm -f gdb.txt
 
-
-rm -f stack.gdb
-
-echo "Running pin"
+cat $OUTFILE
+exit
+echo -e "\n\nRunning pin"
 
 time -p pin -xyzzy -enable_vsm 0 -t $DIR/obj-*/*.so ${@}
 
