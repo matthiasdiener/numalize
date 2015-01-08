e5b7a5c0c49e765fe19fb6e418fbbe642fd39fcf
Matthias Diener <mdiener@inf.ufrgs.br>
2015-01-08 18:44:07 +0100

    allow multiple files

--- page.R
+++ page.R
@@ -13,253 +13,259 @@ options(digits=4, scipen=1000)
 library(data.table)
 
 args = commandArgs(trailingOnly=T)
-if (length(args) != 2)
-	stop("Usage: page.R <page.csv> <#nodes>\n")
+nargs = length(args)
+if (nargs < 2)
+	stop("Usage: page.R <page.csv...> <#nodes>\n")
 
-data = read.csv(args[1])
-nnodes = as.numeric(args[2])
+nnodes = as.numeric(args[nargs])
+files = args[1:(nargs-1)]
 
-# # for larger pages:
-# data$addr = data$addr %/% 512
-# data = data.table(data)
-# data = data[, lapply(.SD, as.numeric)]
-# data = data[, lapply(.SD, sum), by=addr]
-# data = data.frame(data)
+for (filename in files) {
+	cat("##### File:", filename, "\n")
+	data = read.csv(filename)
 
-threads = grep("T\\d+", names(data))
-nthreads = length(threads)
-tpn = nthreads / nnodes
-nodes = c((ncol(data)+1):(ncol(data)+nnodes))
-n = split(threads, ceiling(seq_along(threads)/tpn))
+	# # for larger pages:
+	# data$addr = data$addr %/% 512
+	# data = data.table(data)
+	# data = data[, lapply(.SD, as.numeric)]
+	# data = data[, lapply(.SD, sum), by=addr]
+	# data = data.frame(data)
 
-ttn=c()
-for (i in 1:length(n)) ttn[n[[i]]-2] = i
+	threads = grep("T\\d+", names(data))
+	nthreads = length(threads)
+	tpn = nthreads / nnodes
+	nodes = c((ncol(data)+1):(ncol(data)+nnodes))
+	n = split(threads, ceiling(seq_along(threads)/tpn))
 
-cat("### Input data:\n\n")
-cat("#nodes:", nnodes, "  #threads:", nthreads, "  #threads per node:", tpn, "\n\n")
-cat("thread to node assignment:\n")
-i = 0
-for (x in n) {
-	cat("\tNode ", i, ":", sprintf("%4d", x-3), "  (n=", length(x), ")\n", sep="")
-	i = i+1
+	ttn=c()
+	for (i in 1:length(n)) ttn[n[[i]]-2] = i
+
+	cat("### Input data:\n\n")
+	cat("#nodes:", nnodes, "  #threads:", nthreads, "  #threads per node:", tpn, "\n\n")
+	cat("thread to node assignment:\n")
+	i = 0
+	for (x in n) {
+		cat("\tNode ", i, ":", sprintf("%4d", x-3), "  (n=", length(x), ")\n", sep="")
+		i = i+1
+	}
+
+	cat("\n\n### Properties:\n")
+
+	for (i in 1:length(nodes))
+		data[nodes[i]] = rowSums(data[unlist(n[i])])
+
+	data = data[-threads]
+
+	nodes = c(3:ncol(data))
+
+	data$sum = rowSums(data[nodes])
+	data$max = do.call(pmax, data[nodes])
+
+	data$excl = data$max / data$sum * 100
+
+	npages = nrow(data)
+	total = sum(data$sum) / 100
+
+
+	#### Page mappings
+
+	data$first_touch = ttn[data$firstacc+1]
+
+	data$local_node = max.col(data[nodes], "first")
+
+	mymin = function(x) {
+		ma = which.max(x)
+		mi = tail(which(x==min(x)), 1)
+		mi[which.max(abs(mi-ma))]
+	}
+	data$remote_node = apply(data[nodes], 1, mymin)
+
+	data$rr_node = rep(c(1:nnodes), length.out=npages)
+
+	data$inter_node = (data$addr %% nnodes) + 1
+
+	set.seed(1)
+	data$random_node = floor(runif(npages, 1, nnodes+1))
+
+	data$mixed_node = ifelse(data$excl > 95, data$local_node, data$rr_node)
+
+	totaln = sum(data$sum) / nnodes
+	sums = rep(0, nnodes)
+	new = rep(0, npages)
+
+	for (i in 1:npages) {
+		n = data$local_node[i]
+
+		if (sums[n] >= totaln)
+			n = which.min(sums)
+
+		new[i] = n
+		sums[n] = sums[n] + data$sum[i]
+	}
+	data$bal_node = new
+
+
+	#### Memory Balance
+
+	cat("\nmemory balance (# pages):")
+	cat("\n\tfirst_touch:\t")
+	for (i in 1:nnodes)
+		cat(max(table(data$first_touch)[i],0,na.rm=T), "")
+	cat("\n\tlocal_node:\t")
+	for (i in 1:nnodes)
+		cat(max(table(data$local_node)[i],0,na.rm=T), "")
+	cat("\n\tremote_node:\t")
+	for (i in 1:nnodes)
+		cat(max(table(data$remote_node)[i],0,na.rm=T), "")
+	cat("\n\trr_node:\t")
+	for (i in 1:nnodes)
+		cat(max(table(data$rr_node)[i],0,na.rm=T), "")
+	cat("\n\tinter_node:\t")
+	for (i in 1:nnodes)
+		cat(max(table(data$inter_node)[i],0,na.rm=T), "")
+	cat("\n\trandom_node:\t")
+	for (i in 1:nnodes)
+		cat(max(table(data$random_node)[i],0,na.rm=T), "")
+	cat("\n\tmixed_node:\t")
+	for (i in 1:nnodes)
+		cat(max(table(data$mixed_node)[i],0,na.rm=T), "")
+	cat("\n\tbal_node:\t")
+	for (i in 1:nnodes)
+		cat(max(table(data$bal_node)[i],0,na.rm=T), "")
+
+	cat("\n\nmemory balance (% accesses):")
+	cat("\n\tfirst_touch:\t")
+	amax=0
+	amin=100
+	for (i in 1:nnodes){
+		cat(sprintf("%5.2f ", sum(data$sum[data$first_touch==i], na.rm=T)/total))
+		amax=max(amax, sum(data$sum[data$first_touch==i], na.rm=T)/total)
+		amin=min(amin, sum(data$sum[data$first_touch==i], na.rm=T)/total)
+	}
+	cat("(", 100-(amax-amin), ")", sep="")
+	cat("\n\tlocal_node:\t")
+	amax=0
+	amin=100
+	for (i in 1:nnodes){
+		cat(sprintf("%5.2f ", sum(data$sum[data$local_node==i], na.rm=T)/total))
+		amax=max(amax, sum(data$sum[data$local_node==i], na.rm=T)/total)
+		amin=min(amin, sum(data$sum[data$local_node==i], na.rm=T)/total)
+	}
+	cat("(", 100-(amax-amin), ")", sep="")
+	cat("\n\tremote_node:\t")
+	amax=0
+	amin=100
+	for (i in 1:nnodes){
+		cat(sprintf("%5.2f ", sum(data$sum[data$remote_node==i], na.rm=T)/total))
+		amax=max(amax, sum(data$sum[data$remote_node==i], na.rm=T)/total)
+		amin=min(amin, sum(data$sum[data$remote_node==i], na.rm=T)/total)
+	}
+	cat("(", 100-(amax-amin), ")", sep="")
+	cat("\n\trr_node:\t")
+	amax=0
+	amin=100
+	for (i in 1:nnodes){
+		cat(sprintf("%5.2f ", sum(data$sum[data$rr_node==i], na.rm=T)/total))
+		amax=max(amax, sum(data$sum[data$rr_node==i], na.rm=T)/total)
+		amin=min(amin, sum(data$sum[data$rr_node==i], na.rm=T)/total)
+	}
+	cat("(", 100-(amax-amin), ")", sep="")
+	cat("\n\tinter_node:\t")
+	amax=0
+	amin=100
+	for (i in 1:nnodes){
+		cat(sprintf("%5.2f ", sum(data$sum[data$inter_node==i], na.rm=T)/total))
+		amax=max(amax, sum(data$sum[data$inter_node==i], na.rm=T)/total)
+		amin=min(amin, sum(data$sum[data$inter_node==i], na.rm=T)/total)
+	}
+	cat("(", 100-(amax-amin), ")", sep="")
+	cat("\n\trandom_node:\t")
+	amax=0
+	amin=100
+	for (i in 1:nnodes){
+		cat(sprintf("%5.2f ", sum(data$sum[data$random_node==i], na.rm=T)/total))
+		amax=max(amax, sum(data$sum[data$random_node==i], na.rm=T)/total)
+		amin=min(amin, sum(data$sum[data$random_node==i], na.rm=T)/total)
+	}
+	cat("(", 100-(amax-amin), ")", sep="")
+	cat("\n\tmixed_node:\t")
+	amax=0
+	amin=100
+	for (i in 1:nnodes){
+		cat(sprintf("%5.2f ", sum(data$sum[data$mixed_node==i], na.rm=T)/total))
+		amax=max(amax, sum(data$sum[data$mixed_node==i], na.rm=T)/total)
+		amin=min(amin, sum(data$sum[data$mixed_node==i], na.rm=T)/total)
+	}
+	cat("(", 100-(amax-amin), ")", sep="")
+	cat("\n\tbal_node:\t")
+	amax=0
+	amin=100
+	for (i in 1:nnodes){
+		cat(sprintf("%5.2f ", sum(data$sum[data$bal_node==i], na.rm=T)/total))
+		amax=max(amax, sum(data$sum[data$bal_node==i], na.rm=T)/total)
+		amin=min(amin, sum(data$sum[data$bal_node==i], na.rm=T)/total)
+	}
+	cat("(", 100-(amax-amin), ")", sep="")
+
+
+	#### Exclusivity
+	cat("\n\npage exclusivity (rounded):\n")
+
+	data$excl[is.na(data$excl)] = 100
+	excl_min = ceiling(100/nnodes/10) * 10
+
+	# Round exclusivity and put >, < and %
+	data$excl_round = pmax(pmin(round(data$excl/10)*10, 90), excl_min)
+	data$excl_round = paste(ifelse(data$excl_round==excl_min,"<",""), ifelse(data$excl_round=="90",">",""), data$excl_round, "%", sep="")
+
+	excl = data.frame(data.table(data)[order(excl_round), sum(max), by=excl_round])
+	cat("\t Rounded exclusivity\t number of accesses\n")
+	cat("\t =========================\n")
+	for (i in 1:nrow(excl))
+		cat("\t", excl[i,1], "\t\t", excl[i,2], "\n")
+
+
+	cat("\napplication exclusivity:\n\t", sum(data$max, na.rm=TRUE)/sum(data$sum, na.rm=TRUE)*100, "%\n")
+
+
+	#### Accuracy
+
+	cat("\naccuracy (% pages):\n")
+
+	cat("\tfirst_touch:\t", sprintf("%6.2f\n", sum((data$local_node == data$first_touch))/npages*100))
+	cat("\tlocal_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$local_node))/npages*100))
+	cat("\tremote_node:\t", sprintf("%6.2f\n", sum((data$remote_node == data$local_node))/npages*100))
+	cat("\trr_node:\t", sprintf("%6.2f\n", sum((data$rr_node == data$local_node))/npages*100))
+	cat("\tinter_node:\t", sprintf("%6.2f\n", sum((data$inter_node == data$local_node))/npages*100))
+	cat("\trandom_node:\t", sprintf("%6.2f\n", sum((data$random_node == data$local_node))/npages*100))
+	cat("\tmixed_node:\t", sprintf("%6.2f\n", sum((data$mixed_node == data$local_node))/npages*100))
+	cat("\tbal_node:\t", sprintf("%6.2f\n", sum((data$bal_node == data$local_node))/npages*100))
+
+
+	cat("\naccuracy (% accesses):\n")
+
+	cat("\tfirst_touch:\t", sprintf("%6.2f\n", sum((data$local_node == data$first_touch) * data$sum, na.rm=TRUE)/total))
+	cat("\tlocal_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$local_node) * data$sum, na.rm=TRUE)/total))
+	cat("\tremote_node:\t", sprintf("%6.2f\n", sum((data$remote_node == data$local_node) * data$sum, na.rm=TRUE)/total))
+	cat("\trr_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$rr_node) * data$sum, na.rm=TRUE)/total))
+	cat("\tinter_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$inter_node) * data$sum, na.rm=TRUE)/total))
+	cat("\trandom_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$random_node) * data$sum, na.rm=TRUE)/total))
+	cat("\tmixed_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$mixed_node) * data$sum, na.rm=TRUE)/total))
+	cat("\tbal_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$bal_node) * data$sum, na.rm=TRUE)/total))
+
+
+	#### Write mappings to csv files
+
+	cat("\n\n### Writing mappings to csv files...")
+
+	write.table(data[, c("addr", "first_touch")], file=paste(filename, ".first", sep=""), row.names=F, col.names=F)
+	write.table(data[, c("addr", "local_node")], file=paste(filename, ".local", sep=""), row.names=F, col.names=F)
+	write.table(data[, c("addr", "remote_node")], file=paste(filename, ".remote", sep=""), row.names=F, col.names=F)
+	write.table(data[, c("addr", "rr_node")], file=paste(filename, ".rr", sep=""), row.names=F, col.names=F)
+	write.table(data[, c("addr", "inter_node")], file=paste(filename, ".inter", sep=""), row.names=F, col.names=F)
+	write.table(data[, c("addr", "random_node")], file=paste(filename, ".random", sep=""), row.names=F, col.names=F)
+	write.table(data[, c("addr", "mixed_node")], file=paste(filename, ".mixed", sep=""), row.names=F, col.names=F)
+	write.table(data[, c("addr", "bal_node")], file=paste(filename, ".bal", sep=""), row.names=F, col.names=F)
+
+	cat("  done.\n")
 }
-
-cat("\n\n### Properties:\n")
-
-for (i in 1:length(nodes))
-	data[nodes[i]] = rowSums(data[unlist(n[i])])
-
-data = data[-threads]
-
-nodes = c(3:ncol(data))
-
-data$sum = rowSums(data[nodes])
-data$max = do.call(pmax, data[nodes])
-
-data$excl = data$max / data$sum * 100
-
-npages = nrow(data)
-total = sum(data$sum) / 100
-
-
-#### Page mappings
-
-data$first_touch = ttn[data$firstacc+1]
-
-data$local_node = max.col(data[nodes], "first")
-
-mymin = function(x) {
-	ma = which.max(x)
-	mi = tail(which(x==min(x)), 1)
-	mi[which.max(abs(mi-ma))]
-}
-data$remote_node = apply(data[nodes], 1, mymin)
-
-data$rr_node = rep(c(1:nnodes), length.out=npages)
-
-data$inter_node = (data$addr %% nnodes) + 1
-
-set.seed(1)
-data$random_node = floor(runif(npages, 1, nnodes+1))
-
-data$mixed_node = ifelse(data$excl > 95, data$local_node, data$rr_node)
-
-totaln = sum(data$sum) / nnodes
-sums = rep(0, nnodes)
-new = rep(0, npages)
-
-for (i in 1:npages) {
-	n = data$local_node[i]
-
-	if (sums[n] >= totaln)
-		n = which.min(sums)
-
-	new[i] = n
-	sums[n] = sums[n] + data$sum[i]
-}
-data$bal_node = new
-
-
-#### Memory Balance
-
-cat("\nmemory balance (# pages):")
-cat("\n\tfirst_touch:\t")
-for (i in 1:nnodes)
-	cat(max(table(data$first_touch)[i],0,na.rm=T), "")
-cat("\n\tlocal_node:\t")
-for (i in 1:nnodes)
-	cat(max(table(data$local_node)[i],0,na.rm=T), "")
-cat("\n\tremote_node:\t")
-for (i in 1:nnodes)
-	cat(max(table(data$remote_node)[i],0,na.rm=T), "")
-cat("\n\trr_node:\t")
-for (i in 1:nnodes)
-	cat(max(table(data$rr_node)[i],0,na.rm=T), "")
-cat("\n\tinter_node:\t")
-for (i in 1:nnodes)
-	cat(max(table(data$inter_node)[i],0,na.rm=T), "")
-cat("\n\trandom_node:\t")
-for (i in 1:nnodes)
-	cat(max(table(data$random_node)[i],0,na.rm=T), "")
-cat("\n\tmixed_node:\t")
-for (i in 1:nnodes)
-	cat(max(table(data$mixed_node)[i],0,na.rm=T), "")
-cat("\n\tbal_node:\t")
-for (i in 1:nnodes)
-	cat(max(table(data$bal_node)[i],0,na.rm=T), "")
-
-cat("\n\nmemory balance (% accesses):")
-cat("\n\tfirst_touch:\t")
-amax=0
-amin=100
-for (i in 1:nnodes){
-	cat(sprintf("%5.2f ", sum(data$sum[data$first_touch==i], na.rm=T)/total))
-	amax=max(amax, sum(data$sum[data$first_touch==i], na.rm=T)/total)
-	amin=min(amin, sum(data$sum[data$first_touch==i], na.rm=T)/total)
-}
-cat("(", 100-(amax-amin), ")", sep="")
-cat("\n\tlocal_node:\t")
-amax=0
-amin=100
-for (i in 1:nnodes){
-	cat(sprintf("%5.2f ", sum(data$sum[data$local_node==i], na.rm=T)/total))
-	amax=max(amax, sum(data$sum[data$local_node==i], na.rm=T)/total)
-	amin=min(amin, sum(data$sum[data$local_node==i], na.rm=T)/total)
-}
-cat("(", 100-(amax-amin), ")", sep="")
-cat("\n\tremote_node:\t")
-amax=0
-amin=100
-for (i in 1:nnodes){
-	cat(sprintf("%5.2f ", sum(data$sum[data$remote_node==i], na.rm=T)/total))
-	amax=max(amax, sum(data$sum[data$remote_node==i], na.rm=T)/total)
-	amin=min(amin, sum(data$sum[data$remote_node==i], na.rm=T)/total)
-}
-cat("(", 100-(amax-amin), ")", sep="")
-cat("\n\trr_node:\t")
-amax=0
-amin=100
-for (i in 1:nnodes){
-	cat(sprintf("%5.2f ", sum(data$sum[data$rr_node==i], na.rm=T)/total))
-	amax=max(amax, sum(data$sum[data$rr_node==i], na.rm=T)/total)
-	amin=min(amin, sum(data$sum[data$rr_node==i], na.rm=T)/total)
-}
-cat("(", 100-(amax-amin), ")", sep="")
-cat("\n\tinter_node:\t")
-amax=0
-amin=100
-for (i in 1:nnodes){
-	cat(sprintf("%5.2f ", sum(data$sum[data$inter_node==i], na.rm=T)/total))
-	amax=max(amax, sum(data$sum[data$inter_node==i], na.rm=T)/total)
-	amin=min(amin, sum(data$sum[data$inter_node==i], na.rm=T)/total)
-}
-cat("(", 100-(amax-amin), ")", sep="")
-cat("\n\trandom_node:\t")
-amax=0
-amin=100
-for (i in 1:nnodes){
-	cat(sprintf("%5.2f ", sum(data$sum[data$random_node==i], na.rm=T)/total))
-	amax=max(amax, sum(data$sum[data$random_node==i], na.rm=T)/total)
-	amin=min(amin, sum(data$sum[data$random_node==i], na.rm=T)/total)
-}
-cat("(", 100-(amax-amin), ")", sep="")
-cat("\n\tmixed_node:\t")
-amax=0
-amin=100
-for (i in 1:nnodes){
-	cat(sprintf("%5.2f ", sum(data$sum[data$mixed_node==i], na.rm=T)/total))
-	amax=max(amax, sum(data$sum[data$mixed_node==i], na.rm=T)/total)
-	amin=min(amin, sum(data$sum[data$mixed_node==i], na.rm=T)/total)
-}
-cat("(", 100-(amax-amin), ")", sep="")
-cat("\n\tbal_node:\t")
-amax=0
-amin=100
-for (i in 1:nnodes){
-	cat(sprintf("%5.2f ", sum(data$sum[data$bal_node==i], na.rm=T)/total))
-	amax=max(amax, sum(data$sum[data$bal_node==i], na.rm=T)/total)
-	amin=min(amin, sum(data$sum[data$bal_node==i], na.rm=T)/total)
-}
-cat("(", 100-(amax-amin), ")", sep="")
-
-
-#### Exclusivity
-cat("\n\npage exclusivity (rounded):\n")
-
-data$excl[is.na(data$excl)] = 100
-excl_min = ceiling(100/nnodes/10) * 10
-
-# Round exclusivity and put >, < and %
-data$excl_round = pmax(pmin(round(data$excl/10)*10, 90), excl_min)
-data$excl_round = paste(ifelse(data$excl_round==excl_min,"<",""), ifelse(data$excl_round=="90",">",""), data$excl_round, "%", sep="")
-
-excl = data.frame(data.table(data)[order(excl_round), sum(max), by=excl_round])
-cat("\t Rounded exclusivity\t number of accesses\n")
-cat("\t =========================\n")
-for (i in 1:nrow(excl))
-	cat("\t", excl[i,1], "\t\t", excl[i,2], "\n")
-
-
-cat("\napplication exclusivity:\n\t", sum(data$max, na.rm=TRUE)/sum(data$sum, na.rm=TRUE)*100, "%\n")
-
-
-#### Accuracy
-
-cat("\naccuracy (% pages):\n")
-
-cat("\tfirst_touch:\t", sprintf("%6.2f\n", sum((data$local_node == data$first_touch))/npages*100))
-cat("\tlocal_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$local_node))/npages*100))
-cat("\tremote_node:\t", sprintf("%6.2f\n", sum((data$remote_node == data$local_node))/npages*100))
-cat("\trr_node:\t", sprintf("%6.2f\n", sum((data$rr_node == data$local_node))/npages*100))
-cat("\tinter_node:\t", sprintf("%6.2f\n", sum((data$inter_node == data$local_node))/npages*100))
-cat("\trandom_node:\t", sprintf("%6.2f\n", sum((data$random_node == data$local_node))/npages*100))
-cat("\tmixed_node:\t", sprintf("%6.2f\n", sum((data$mixed_node == data$local_node))/npages*100))
-cat("\tbal_node:\t", sprintf("%6.2f\n", sum((data$bal_node == data$local_node))/npages*100))
-
-
-cat("\naccuracy (% accesses):\n")
-
-cat("\tfirst_touch:\t", sprintf("%6.2f\n", sum((data$local_node == data$first_touch) * data$sum, na.rm=TRUE)/total))
-cat("\tlocal_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$local_node) * data$sum, na.rm=TRUE)/total))
-cat("\tremote_node:\t", sprintf("%6.2f\n", sum((data$remote_node == data$local_node) * data$sum, na.rm=TRUE)/total))
-cat("\trr_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$rr_node) * data$sum, na.rm=TRUE)/total))
-cat("\tinter_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$inter_node) * data$sum, na.rm=TRUE)/total))
-cat("\trandom_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$random_node) * data$sum, na.rm=TRUE)/total))
-cat("\tmixed_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$mixed_node) * data$sum, na.rm=TRUE)/total))
-cat("\tbal_node:\t", sprintf("%6.2f\n", sum((data$local_node == data$bal_node) * data$sum, na.rm=TRUE)/total))
-
-
-#### Write mappings to csv files
-
-cat("\n\n### Writing mappings to csv files...")
-
-write.table(data[, c("addr", "first_touch")], file=paste(args[1], ".first", sep=""), row.names=F, col.names=F)
-write.table(data[, c("addr", "local_node")], file=paste(args[1], ".local", sep=""), row.names=F, col.names=F)
-write.table(data[, c("addr", "remote_node")], file=paste(args[1], ".remote", sep=""), row.names=F, col.names=F)
-write.table(data[, c("addr", "rr_node")], file=paste(args[1], ".rr", sep=""), row.names=F, col.names=F)
-write.table(data[, c("addr", "inter_node")], file=paste(args[1], ".inter", sep=""), row.names=F, col.names=F)
-write.table(data[, c("addr", "random_node")], file=paste(args[1], ".random", sep=""), row.names=F, col.names=F)
-write.table(data[, c("addr", "mixed_node")], file=paste(args[1], ".mixed", sep=""), row.names=F, col.names=F)
-write.table(data[, c("addr", "bal_node")], file=paste(args[1], ".bal", sep=""), row.names=F, col.names=F)
-
-cat("  done.\n")
