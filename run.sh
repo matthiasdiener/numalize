33fcaca649bfb65a9658a31d7fbf38266541bf4e
Matthias Diener <mdiener@inf.ufrgs.br>
2015-01-22 14:52:47 -0200

    only run gdb when page maps requested

--- run.sh
+++ run.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-set -o errexit; set -o nounset
+set -o errexit -o nounset -o pipefail
 
-# directory of run.sh
+# directory of this script
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# randomizing the virtual address space needs to be off for correct traces
-VA_RANDOM=$(sysctl -n kernel.randomize_va_space)
-if [ $VA_RANDOM -ne 0 ]; then
-	echo "sysctl kernel.randomize_va_space needs to be 0 for a correct trace (it is $VA_RANDOM currently)"
-	echo "exiting"
-	exit 1
-fi
 
 # recompile pintool if necessary
 (cd $DIR; make -q || make)
 
+# program to trace and its arguments
 PROGARGS=$(echo ${@} | sed s,.*--\ ,,)
 PROG=$(echo $PROGARGS | { read first rest; echo $(basename $first) | sed s,\\s.*,, ; } )
-OUTFILE=$PROG.stackmap
 
-PAGESIZE=$(getconf PAGESIZE)
+if [[ $1 == "-p" ]]; then # page maps requested, need to generate correct stack addresses with gdb
 
-echo -e "## gathering stack information via gdb\n"
+	# randomizing the virtual address space needs to be off for correct page traces
+	VA_RANDOM=$(sysctl -n kernel.randomize_va_space)
+	if [ $VA_RANDOM -ne 0 ]; then
+		echo "sysctl kernel.randomize_va_space needs to be 0 for a correct trace (it is $VA_RANDOM currently)"
+		echo "exiting"
+		exit 1
+	fi
 
-# create stack script
-cat > stack.gdb << EOF
-python
+	OUTFILE=$PROG.stackmap
+
+	PAGESIZE=$(getconf PAGESIZE)
+
+	echo -e "## gathering stack information via gdb\n"
+
+	# create stack script
+	cat > stack.gdb << EOF
+	python
 import os, math, subprocess, sys
 pagebits = int(math.log($PAGESIZE, 2))
 f = open("$OUTFILE", 'w')
@@ -79,21 +83,20 @@ f.close()
 end
 EOF
 
-# run gdb with stack script
-gdb --batch-silent --command=stack.gdb --args $PROGARGS
+	# run gdb with stack script
+	gdb --batch-silent --command=stack.gdb --args $PROGARGS
 
-rm -f stack.gdb
-
+	rm -f stack.gdb
+fi
 
 # finally, run pin
-echo -e "\n\n## running pin"
+echo -e "\n\n## running pin: $PROGARGS"
 
 time -p pin -xyzzy -enable_vsm 0 -t $DIR/obj-*/*.so ${@}
 
-# rm -f $OUTFILE ${OUTFILE}2
-
-
-# sort output page csv according to page address
-for f in $PROG.*.page.csv; do
-	sort -n -t, -k 1,1 -o $f $f
-done
+if [[ $1 == "-p" ]]; then
+	# sort output page csv's according to page address
+	for f in $PROG.*.page.csv; do
+		sort -n -t, -k 1,1 -o $f $f
+	done
+fi
