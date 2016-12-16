97651fdce0d1e9ff7859715736a6b55feedb729f
Matthias Diener <mdiener@inf.ufrgs.br>
2016-12-16 18:04:24 -0200

    mse in julia

---
+++ scripts/mse.jl
@@ -0,0 +1,32 @@
+#!/usr/bin/env julia
+
+function mse(A, B)
+    if size(A) != size(B)
+        error("Input matrices do not have same dimensions")
+    end
+
+    if size(A)[1] != size(A)[2]
+        error("Input matrices are not square")
+    end
+
+    nt = size(A)[1]
+
+    for i in 1:nt
+        A[i,nt+1-i] = 0
+        B[i,nt+1-i] = 0
+    end
+
+    A = A/maximum(A) * 100
+    B = B/maximum(B) * 100
+
+    mse = mean((A - B).^2)
+end
+
+if length(ARGS) != 2
+    error("Usage: ", PROGRAM_FILE, " <comm_pattern_A.csv> <comm_pattern_B.csv>")
+end
+
+mat1 = readdlm(ARGS[1], ',')
+mat2 = readdlm(ARGS[2], ',')
+
+println(mse(mat1, mat2))
