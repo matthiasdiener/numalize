b4cfa74c048d6e06dcf508908e238dae8e518b7c
Matthias Diener <mdiener@illinois.edu>
2017-02-18 22:30:24 -0200

    make sure page csv is sorted even with nonzero exit code

--- run.sh
+++ run.sh
@@ -18,7 +18,10 @@ PROG=$(echo $PROGARGS | { read first rest; echo $(basename $first) | sed s,\\s.*
 # Run pin
 echo "### running pin: $PROGARGS"
 
+set +o errexit
 time -p $PIN_ROOT/pin -t $DIR/obj-*/numalize.so ${@}
+retcode=$?
+set -o errexit
 
 if [[ $1 == "-p" ]]; then
 	# sort output page csv's according to page address
@@ -26,3 +29,5 @@ if [[ $1 == "-p" ]]; then
 		sort -n -t, -k 1,1 -o $f $f
 	done
 fi
+
+exit $retcode
